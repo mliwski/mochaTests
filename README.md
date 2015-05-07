be525eacee2444ea9f69857ef08bfb91a14f36f0
mliwski <matias.liwski@avantrip.com>
2015-05-07 12:48:14 -0300

    First release

---
+++ README.md
@@ -0,0 +1,19 @@
+# Tests de mocha, chai, chai-as-promised, webstorm, etc
+
+## Recursos
+* [Mocha - Test framework](http://mochajs.org/)
+* [Chai - BDD / TDD assertion library for node and the browser](http://chaijs.com/)
+* [Chai - BDD assertions library](http://chaijs.com/api/bdd/)
+* [Chai - Assertions for Promises](http://chaijs.com/plugins/chai-as-promised)
+
+
+## A tener en cuenta
+* Si utilizo should aunque quede mas cheto no funca muy bien en browsers
+* Al utilizar chais-as-prmised, no pasar el done a la function de it o no resuelve la Promise
+
+# Siguientes pasos
+1. Gruntjs
+1. JSHint
+1. Incluir modulos propios
+1. Mocks (sinon?)
+1. Mock de dependencia de modulos (rewire?/spy?)
---
+++ test/base/test.js
@@ -0,0 +1,39 @@
+var chai = require('chai');
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+
+var BluePromise = require('bluebird');
+
+var beverages_a = { tea: [ 'chai', 'matcha', 'oolong' ] };
+var beverages_b = { tea: [ 'chai', 'matcha', 'oolong' ] };
+
+before(function(){
+	console.log('Inicio de los test (o por ejemplo levantar la conexion a la db)');
+});
+
+describe('Testing ', function(){
+  describe('Sync behavior', function(){
+    it('should berages be deeply equal', function(){
+		expect(beverages_a).to.be.deep.equal(beverages_b);
+    })
+  })
+  
+  describe('Async behavior', function(){
+    it('should berages be deeply equal after timeout using done', function(done){
+		setTimeout(function(){
+			expect(beverages_a).to.be.deep.equal(beverages_b);
+			done();
+		},120);
+    })
+	
+	it('should berages be deeply equal using promises', function(){
+		//expect(BluePromise.resolve({ foo: "bar" })).to.eventually.have.property("foo");
+		expect(BluePromise.resolve(beverages_a)).to.be.eventually.deep.equal(beverages_b);
+    })
+  })
+});
+
+after(function(){
+	console.log('Cierre de los test (o por ejemplo borrar los ejemplos de db y cerrar la conexion)');
+});
